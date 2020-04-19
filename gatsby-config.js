938e515340d7ef63d2ac740480dc588642001389
Ashish Santikari <mailto.ashishsan@gmail.com>
2020-04-19 18:55:21 +0200

    firebase config

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,5 +74,19 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.FIREBASE_DB_URL,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.FIREBASE_APP_ID,
+        }
+      }
+    }
   ],
-}
+};
